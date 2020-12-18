eb1761f0d0bc3c4ada400908f65ddc752b73398a
ulisse-fontanot <ulissef99@gmail.com>
2020-12-18 10:14:14 +0100

    init-es-1

---
+++ es-1/js/script.js
@@ -0,0 +1,21 @@
+var array = [];
+
+// for
+// for (var i = 0; i < 5; i++) {
+//   var numero = parseInt(prompt("inserisci un numero"));
+//   array.push(numero);
+// }
+//
+// somma = array[0] + array[1]+ array[2]+ array[3]+ array[4];
+// console.log(somma);
+
+//while
+var i = 0;
+while (i < 5) {
+  i++;
+  var numero = parseInt(prompt("inserisci un numero"));
+  array.push(numero);
+}
+
+somma = array[0] + array[1]+ array[2]+ array[3]+ array[4];
+console.log(somma);
